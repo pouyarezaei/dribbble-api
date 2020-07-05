fa8bfe0bd0f83ebd715e8fe2cd988892c6d940f4
pouya rezaei <pouyarezaei810@gmail.com>
2020-07-06 01:18:25 +0430

    remove duplicate methods

--- app/Http/Controllers/ShotController.php
+++ app/Http/Controllers/ShotController.php
@@ -5,59 +5,144 @@ namespace App\Http\Controllers;
 
 
 use App\Http\Resources\ShotsResource;
+use App\Repositories\ShotRepository\ShotRepositoryInterface;
 use App\Shot;
 use App\Tag;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 /**
- * @group shots
+ * @group Shots
  *
- * Api for shots
+ *  Api for shots
  */
 class ShotController extends ApiController
 {
-    public function getShotById($id)
+    protected $shotRepository;
+
+    public function __construct(ShotRepositoryInterface $shotRepository)
     {
-        $shot = Shot::find($id);
-        if (!is_null($shot)) {
-            return $shot;
-        }
-        // TODO some error for not founded
+        $this->shotRepository = $shotRepository;
     }
 
-    public function updateShotById(Request $request, $id)
+    /**
+     * @method Get
+     * @queryParam  page required The page number. Example: 4
+     */
+    public function getAll()
     {
-        $shot = Shot::find($id);
-        if (!is_null($shot)) {
-            return $shot->update($request->all());
-        }
-        // TODO not found error
+        // TODO fix page size
+        // TODO dont forget redis cache
+        return ShotsResource::collection($this->shotRepository->allWithPages());
     }
 
+    /**
+     * @method Get
+     * @param int shot_id $id
+     * @urlParam  id required The id of the shot. Example: 1
+     * @return \Illuminate\Http\JsonResponse|JsonResource
+     */
+    public function getById($id)
+    {
+        $shot = $this->shotRepository->find($id);
+        if (!is_null($shot)) {
+
+            return new ShotsResource($shot);
+        }
+
+        return $this->respondNotFoundError();
+
+    }
+
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(), Shot::rules());
+        if (!$validate->fails()) {
+            $this->shotRepository->store(
+                [
+                    'title' => $request->input('title'),
+                    'description' => $request->input('description')
+                ]
+            );
+            return $this->respondSuccess();
+        }
+        return $this->respondValidateError($validate->errors());
+    }
+
+    /**
+     * @method Put
+     * @param Request $request
+     * @param int shot_id $id
+     * @urlParam  id required The id of the shot. Example: 1
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function updateById(Request $request, $id)
+    {
+        $shot = $this->shotRepository->find($id);
+        $user = Auth::user();
+        if (!is_null($shot) && !is_null($user)) {
+            if ($shot->user_id == $user->id) {
+                $shot->update(request()->all());
+                return $this->respondSuccess();
+            }
+            return $this->respondUnauthorized();
+        }
+        return $this->respondNotFoundError();
+    }
+
+    /**
+     * @method Delete
+     * @param int shot_id $id
+     * @return \Illuminate\Http\JsonResponse
+     * @urlParam  id required The id of the shot. Example: 1
+     */
+    public function deleteById($id)
+    {
+        $shot = $this->shotRepository->find($id);
+        $user = Auth::user();
+        if (!is_null($shot) && !is_null($user)) {
+            if ($shot->user_id == $user->id) {
+                $this->shotRepository->delete($id);
+                return $this->respondSuccess();
+            }
+            return $this->respondUnauthorized();
+        }
+        return $this->respondNotFoundError();
+    }
+
+    /**
+     * @param Request $request
+     * @param int shot_id $id
+     * @urlParam  id required The id of the shot. Example: 1
+     */
     public function updateShotTagsById(Request $request, $id)
     {
+        // TODO Updating a shot requires the user to be authenticated with the upload scope.
+        // TODO The authenticated user must also own the shot.
+        // TODO not found error
+
         $shot = Shot::find($id);
-
-
         if (!is_null($shot)) {
             $tag = Tag::whereIn('tag', $request['tags'])->get();
             $shot->tags()->sync($tag);
         }
-        // TODO not found error
     }
 
-    public function getAllShots()
-    {
-//        $data = Cache::remember('shots', 60, function () {
-            return new ShotsResource(Shot::with(['user', 'comments', 'images', 'videos', 'gifs', 'tags'])->orderBy('update_at')->paginate(1));
-//        });
-
-
-    }
-
+    /**
+     * @param Request $request
+     * @param int shot_id $id
+     * @urlParam  id required The id of the shot. Example: 1
+     */
     public function addMedia(Request $request, $id)
     {
+        // TODO Updating a shot requires the user to be authenticated with the upload scope.
+        // TODO The authenticated user must also own the shot.
+        // TODO check not found shot
+        // TODO file not supports error
+        // TODO file not exist error
+
         if ($request->hasFile('file')) {
             $shot = Shot::find($id);
             $file = $request->file('file');
@@ -78,21 +163,24 @@ class ShotController extends ApiController
                     $file->move('shots/videos', $fileName);
                     break;
                 default:
-                    // TODO file not supports
             }
-        } else {
-            // TODO file not exist
         }
-
     }
 
+    /**
+     * @param int shot_id $id
+     * @urlParam  id required The id of the shot. Example: 1
+     * @return
+     */
     public function getCommentByShotId($id)
     {
+        // TODO not found error
+
         $shot = Shot::find($id);
         if (!is_null($shot)) {
             return $shot->comments;
         }
-        // TODO some error for not founded
     }
+
 
 }
