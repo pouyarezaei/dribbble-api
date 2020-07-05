dd3f3d64c884da841e4aa133596a7c5621341ece
pouya rezaei <pouyarezaei810@gmail.com>
2020-07-06 01:21:08 +0430

    convert fake size 500 to 20

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         User::truncate();
-        factory(User::class, 500)->create();
+        factory(User::class, 20)->create();
     }
 }
