97cc025c21d18c5c80a7a00a9615590879b6338f
pouya rezaei <pouyarezaei810@gmail.com>
2020-07-06 01:09:52 +0430

    add         JsonResource::withoutWrapping

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,4 +16,10 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    public function boot()
+    {
+        JsonResource::withoutWrapping();
+    }
+
 }
